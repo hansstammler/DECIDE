f55830f42631527e6940fe899d7276434d79f6d8
Oscar Olsson <oscols@kth.se>
2023-01-30 10:45:33 +0100

    fix: Changed if statement to greater than instead of greater or equal than AREA1. All tests pass (issue #38)

--- app/src/main/java/decide/LICS.java
+++ app/src/main/java/decide/LICS.java
@@ -76,7 +76,7 @@ public class LICS {
 			Point b = globals.POINTS[i + 1];
 			Point c = globals.POINTS[i + 2];
 			double area = (a.x*(b.y - c.y) + b.x*(c.y - a.y) + c.x*(a.y - b.y))/2;
-			if(area >= params.AREA1)
+			if(area > params.AREA1)
 				return true;
 		}
 		return false;
