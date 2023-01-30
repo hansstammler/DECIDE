323ee71e208c21ada94ce80b966d54b0ffcf9107
Oscar Olsson <oscols@kth.se>
2023-01-30 10:28:00 +0100

    doc: Added javadoc to LIC4 (issue #8)

--- app/src/main/java/decide/LICS.java
+++ app/src/main/java/decide/LICS.java
@@ -60,6 +60,17 @@ public class LICS {
 		return false;
 	}
 
+	/**
+	 * There exists at least one set of Q PTS consecutive data points that lie in more than QUADS
+	 * quadrants. Where there is ambiguity as to which quadrant contains a given point, priority
+	 * of decision will be by quadrant number, i.e., I, II, III, IV. For example, the data point (0,0)
+	 * is in quadrant I, the point (-l,0) is in quadrant II, the point (0,-l) is in quadrant III, the point
+	 * (0,1) is in quadrant I and the point (1,0) is in quadrant I.
+	 * 
+	 * @param globals Instance of InputVariables
+	 * @param params Instance of Parameters
+	 * @return true or false
+	 */
 	static boolean four(InputVariables globals, Parameters params) {
 		boolean[] inQuadrant = new boolean[5];
 		int numberOfQuadrants = 0;
