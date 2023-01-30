5f1c52745eb92304184da986de8ba4e684165a2e
Oscar Olsson <oscols@kth.se>
2023-01-30 10:07:05 +0100

    feat: Added test for LIC4, when the points do not lie in more than QUADS quadrants it should return false (issue #8)

--- app/src/test/java/decide/LICSTest.java
+++ app/src/test/java/decide/LICSTest.java
@@ -48,6 +48,15 @@ public class LICSTest {
     }
 
     @Test
+    void LIC4IsFalseWhenQ_PTSPointsLieInLessThanQUADSQuadrants() {
+        params.Q_PTS = 2;
+        params.QUADS = 2;
+        Point[] points = {new Point(1, 1), new Point(1, 2), new Point(0.5, 0.5)};
+        globals.POINTS = points;
+        assertFalse(LICS.four(globals, params), "LIC4 should output false when Q_PTS consecutive data points lie in less than QUADS quadrants");
+    }
+
+    @Test
     @DisplayName("LIC 2 should return correct boolean")
     void LIC2IsCorrect() {
         params.EPSILON = params.PI/2;
