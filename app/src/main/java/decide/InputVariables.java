70f093818b7a8dc0bbcf9a8b787f812e824eb438
hansstammler <stammlerhans@gmail.com>
2023-01-30 22:03:36 +0100

    fix: changed LCM to proper type #46
    Switched LCM from byte array to instance of LogicalConnectorMatrix

--- app/src/main/java/decide/InputVariables.java
+++ app/src/main/java/decide/InputVariables.java
@@ -3,6 +3,24 @@ public class InputVariables {
     int NUMPOINTS;
     Point[] POINTS;
     Parameters PARAMETERS;
-    byte[][] LCM;
+    LogicalConnectorMatrix LCM;
     boolean[] PUV;
+
+    /**
+     * This initializes some deterministic instance of InputVariables
+     */
+    public void initialize(){
+        NUMPOINTS = 50;
+
+        POINTS = new Point[NUMPOINTS];
+        for(int i = 0; i < NUMPOINTS; i++) POINTS[i] = new Point(i, i);
+
+        PARAMETERS = new Parameters();
+        PARAMETERS.initialize();
+
+        LCM = new LogicalConnectorMatrix('d');
+        
+        PUV = new boolean[15];
+        for(int i = 0; i < 15; i++) PUV[i] = (i % 2 == 0);
+    }
 }
